254e05cee2c916d99cf1a96020fccd4124581165
Benjamin <benjamin@connectica.no>
2021-04-24 17:05:37 +0700

    added all internal configs to the project via printev. Note that other customers will have to set these valiables in shell ENV for now, will implement another sollution later

--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,7 @@ import * as vscode from 'vscode'
 import SetUserAuthentication from './UseCases/Commands/SetUserAuthentication'
 import Test from './UseCases/Commands/Test'
 import JiraTest from './UseCases/Commands/JiraTest'
-import Initiallize from './UseCases/Commands/Initiallize'
-import Shell from './UseCases/Commands/Shell'
+//import Shell from './UseCases/Commands/Shell'
 import Config from './UseCases/Commands/Config'
 
 
@@ -17,7 +16,16 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 
+	context.globalState.update('JIRA_OLD_API_URL','https://morphosis.atlassian.net/rest/agile/1.0/')
 	Config("JIRA_USERNAME","git config user.email",context)
+	Config("JIRA_PASSWORD","printenv JIRA_API_KEY",context)
+	Config("HARVEST_ACCESS_TOKEN","printenv HARVEST_ACCESS_TOKEN",context)
+	Config("HARVEST_ACCOUNT_ID","printenv HARVEST_ACCOUNT_ID",context)
+	Config("HARVEST_ACCESS_TOKEN","printenv HARVEST_ACCESS_TOKEN",context)
+	Config("FRESHTEAM_API_KEY","printenv FRESHTEAM_API_KEY",context)
+	Config("SLACK_TICKET_HOOK_PRODUCTION","printenv SLACK_TICKET_HOOK_PRODUCTION",context)
+
+
 
 	// This sets the command in the statusbar
 	const statusbar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100)
