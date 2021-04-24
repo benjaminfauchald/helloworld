7b572a081809aa6fc4ce5113ddee8644b8357a42
Benjamin <benjamin@connectica.no>
2021-04-24 18:18:39 +0700

    cleand up into modules

--- src/extension.ts
+++ src/extension.ts
@@ -7,50 +7,9 @@ import JiraTest from './UseCases/Commands/JiraTest'
 //import Shell from './UseCases/Commands/Shell'
 import Config from './UseCases/Commands/Config'
 
-import './TreeDataProvider'
+import TreeDataProvider from './TreeDataProvider'
 
 
-
-
-class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
-	onDidChangeTreeData?: vscode.Event<TreeItem|null|undefined>|undefined;
-  
-	data: TreeItem[];
-  
-	constructor() {
-	  this.data = [new TreeItem('cars', [
-		new TreeItem(
-			'Ford', [new TreeItem('Fiesta'), new TreeItem('Focus'), new TreeItem('Mustang')]),
-		new TreeItem(
-			'BMW', [new TreeItem('320'), new TreeItem('X3'), new TreeItem('X5')])
-	  ])];
-	}
-  
-	getTreeItem(element: TreeItem): vscode.TreeItem|Thenable<vscode.TreeItem> {
-	  return element;
-	}
-  
-	getChildren(element?: TreeItem|undefined): vscode.ProviderResult<TreeItem[]> {
-	  if (element === undefined) {
-		return this.data;
-	  }
-	  return element.children;
-	}
-  }
-  
-  class TreeItem extends vscode.TreeItem {
-	children: TreeItem[]|undefined;
-  
-	constructor(label: string, children?: TreeItem[]) {
-	  super(
-		  label,
-		  children === undefined ? vscode.TreeItemCollapsibleState.None :
-								   vscode.TreeItemCollapsibleState.Expanded);
-	  this.children = children;
-	}
-  }
-  
-  
 
 
 
