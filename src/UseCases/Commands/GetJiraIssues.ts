ff56ba13e752826fabed872c8747a9e4405d6a80
Benjamin <benjamin@connectica.no>
2021-04-25 13:06:57 +0700

    hmm... having trouble wiith async

--- src/UseCases/Commands/GetJiraIssues.ts
+++ src/UseCases/Commands/GetJiraIssues.ts
@@ -1,8 +1,12 @@
 import axios from 'axios'
 import Jira from '../../Entities/Jira'
-import IssueInterface from '../../Entities/Interfaces/IssueInterface'
+import JiraIssueInterface from '../../Entities/Interfaces/JiraIssueInterface'
+import TaskProvider from '../../lib/TaskProvider'
+import { resolve } from 'path'
 
-const GetJiraIssues = async (): Promise<IssueInterface> => {
+const GetJiraIssues = async (): Promise<any> => {
+
+//const GetJiraIssues = async (): Promise<JiraIssueInterface> => {
   const jira = new Jira()
   const key = "MOPS-10"
   const url = `https://morphosis.atlassian.net/rest/api/3/issue/${key}/?fields=timetracking`
@@ -16,18 +20,22 @@ const GetJiraIssues = async (): Promise<IssueInterface> => {
   } catch (err) {
     console.log(err)
   }
-
-  const issue = {
-    id: userResponse?.data?.id || 0,
-    key: userResponse?.data?.key || '',
-    issueType: userResponse?.data?.key || '',
-    project: userResponse?.data?.key || ''
-  }
+  
+    const issue = {
+      id: userResponse?.data?.id || 0,
+      key: userResponse?.data?.key || '',
+      issueType: userResponse?.data?.key || '',
+      project: userResponse?.data?.key || ''
+    }
 
   console.log `ID: ${issue.id}`
   console.log `Key: ${issue.key}`
  
-  return issue
+
+//
+  console.log(await userResponse?.data)
+
+
 
 }
 
--- src/lib/TaskProvider.ts
+++ src/lib/TaskProvider.ts
@@ -1,5 +1,14 @@
 import * as vscode from 'vscode';
+import axios from 'axios'
+import Jira from '../Entities/Jira'
 import TreeDataProvider from './TreeDataProvider'
+import GetJiraIssues from '../UseCases/Commands/GetJiraIssues'
+import JiraIssueInterface from '../Entities/Interfaces/JiraIssueInterface'
+
+import JiraTest from '../UseCases/Commands/JiraTest'
+
+import { tasks } from 'vscode';
+
 
 export class TaskProvider implements vscode.TreeDataProvider<TreeTask> {
   
@@ -9,21 +18,45 @@ export class TaskProvider implements vscode.TreeDataProvider<TreeTask> {
 
     public async getChildren(task?: TreeTask): Promise<TreeTask[]> {
   
-        let tasks = await vscode.tasks.fetchTasks().then(function (value) {
-            return value;
-        });
+        // let tasks = await vscode.tasks.fetchTasks().then(function (value) {
+        //     return value;
+        // });
       
+
+
+
+        console.log(GetJiraIssues())
+        console.log(JiraTest())
+        
+
+
+        let tasks = ["mps10","mps12","mps150","mps13","mps1550"]
+
         let treeTasks: TreeTask[] = [];
-      
+
         if (tasks.length !== 0) {
           for (var i = 0; i < tasks.length; i++ ) {                             
-            treeTasks[i] = new TreeTask(tasks[i].definition.type, tasks[i].name, vscode.TreeItemCollapsibleState.None, { 
+            treeTasks[i] = new TreeTask(tasks[i], tasks[i], vscode.TreeItemCollapsibleState.None, { 
                   command: 'taskOutline.executeTask', 
                   title: "Execute", 
                   arguments: [tasks[i], ]
              });
            }
         }
+
+        //   let treeTasks: TreeTask[] = [];
+        // if (tasks.length !== 0) {
+        //   for (var i = 0; i < tasks.length; i++ ) {                             
+        //     treeTasks[i] = new TreeTask(tasks[i].definition.type, tasks[i].name, vscode.TreeItemCollapsibleState.None, { 
+        //           command: 'taskOutline.executeTask', 
+        //           title: "Execute", 
+        //           arguments: [tasks[i], ]
+        //      });
+        //    }
+        // }
+
+
+        
         return treeTasks;
     }
 
