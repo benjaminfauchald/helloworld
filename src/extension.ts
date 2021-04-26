099ac5e997497da7cc0165e83fd9ad51ac3e2210
Benjamin <benjamin@connectica.no>
2021-04-26 13:25:00 +0700

    Got Autologin to harvest to work, and can now get the user and the projects in the initializer

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
+import axios from 'axios'
 
 import Config from './UseCases/Commands/Config'
 import SetUserAuthentication from './UseCases/Commands/SetUserAuthentication'
@@ -12,6 +13,21 @@ import GetJiraIssues from './UseCases/Commands/GetJiraIssues'
 import Hello from './UseCases/Commands/Hello'
 import TreeDataProvider from './lib/TreeDataProvider'
 import NodeDependenciesProvider from './lib/NodeDependenciesProvider'
+
+import Harvest from "./Entities/Harvest"
+import ProjectInterface from "./Entities/Interfaces/ProjectInterface"
+import UserInterface from './Entities/Interfaces/UserInterface';
+import Project from "./Entities/Project"
+import ProjectCollection from "./Entities/ProjectCollection"
+import User from "./Entities/User"
+import getProjectsAssignments from "./UseCases/getProjectsAssignments"
+import getUser from "./UseCases/getUser"
+
+
+
+
+
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -29,11 +45,9 @@ export async function activate(context: vscode.ExtensionContext) {
 	Config("FRESHTEAM_API_KEY","printenv FRESHTEAM_API_KEY",context)
 	Config("SLACK_TICKET_HOOK_PRODUCTION","printenv SLACK_TICKET_HOOK_PRODUCTION",context)
 
-
-
 	// Logging into harvest
-	SetUserAuthentication
-	SetupHarvest
+	SetUserAuthentication(context)
+	SetupHarvest(context)
 
 
 
@@ -49,20 +63,58 @@ export async function activate(context: vscode.ExtensionContext) {
 	vscode.window.registerTreeDataProvider('taskOutline', new TreeDataProvider());
 
 	// Trying to set command on tree view
-	vscode.commands.registerCommand("taskOutline.selectNode", (item:vscode.TreeItem) => {
+
+
+	vscode.commands.registerCommand("taskOutline.selectNode", async (item:vscode.TreeItem) => {
+		console.clear() 
 		console.log(item.label);
-	});
+		console.log(item.label!.toString().split('/')[0]);
+
+
+		// vscode.window
+		// 	.showInformationMessage('hello', ...['test', 'taco', 'cheeseburger'])
+		// 	.then(selection => {
+		// 		console.log(selection);
+		// 	});
+
+		let me:any = await getUser() 
+		if (!me.email) return
+
+		console.log(`that was easy${me.email}`)
+		console.log(`that was easy${me.firstName}`)
+
+
+
+		let projectCollection = new ProjectCollection()
+		const isHarvestSetup = await SetupHarvest(context)
+		if (!isHarvestSetup) return
+		
+		const projectNames = projectCollection.elements.map((p: Project) => {
+			return p.name
+		})
+
+		//console.log(`projectNames ${projectNames}`);
+
+
+		// vscode.window.showQuickPick(projectNames, {
+		// 	ignoreFocusOut: true,
+		// 	placeHolder: 'Choose a Project'
+		// })
+
+
+
+	})
+
 
 	// This sets the command in the statusbar
 	const statusbar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100)
 	statusbar.text = 'Morphois'
-	statusbar.command = 'harvest-vscode.login'
+//	statusbar.command = 'harvest-vscode.login'
 	statusbar.show()
 
 	// The command has been defined in the package.json file
 
 	const commands: vscode.Disposable[] = [
-		SetUserAuthentication(context),
 		JiraTest(context)		
 	]
 
